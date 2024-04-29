2e855f439c51ffde875383a498c8ce1222609a9c
MutisoMuli <mulimutiso525@gmail.com>
2024-04-29 03:33:15 +0300

    Done

--- royalgems_frontend/src/modules/home/components/hero/index.tsx
+++ royalgems_frontend/src/modules/home/components/hero/index.tsx
@@ -3,7 +3,7 @@ import { Button, Heading } from "@medusajs/ui"
 
 const Hero = () => {
   return (
-    <div className="h-[90vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle" style={{ backgroundImage: "url('https://res.cloudinary.com/dhbztjzkr/image/upload/v1713929732/image_2_gndmac.jpg')", backgroundSize: "100% auto", backgroundRepeat: "no-repeat", backgroundPosition: "center center" }}>
+    <div className="h-[60vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle" style={{ backgroundImage: "url('https://res.cloudinary.com/dhbztjzkr/image/upload/v1713929732/image_2_gndmac.jpg')", backgroundSize: "100% auto", backgroundRepeat: "no-repeat", backgroundPosition: "center center" }}>
       <img src="https://res.cloudinary.com/dhbztjzkr/image/upload/v1713957203/royalgems_logo-removebg_rpnnvv.png" alt="Royal Gems Logo" style={{ height: "150px", width: "150px", margin: "auto"}} />
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6"></div>
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
