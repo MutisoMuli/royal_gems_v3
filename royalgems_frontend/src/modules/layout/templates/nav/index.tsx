40b8cb25647e6c748a2851f0d3768178d1da9c78
MutisoMuli <mulimutiso525@gmail.com>
2024-05-04 09:56:31 +0300

    Done

--- royalgems_frontend/src/modules/layout/templates/nav/index.tsx
+++ royalgems_frontend/src/modules/layout/templates/nav/index.tsx
@@ -4,6 +4,8 @@ import LocalizedClientLink from "@modules/common/components/localized-client-lin
 import CartButton from "@modules/layout/components/cart-button"
 import SideMenu from "@modules/layout/components/side-menu"
 import styles from './Nav.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 export default async function Nav() {
   const regions = await listRegions().then((regions) => regions)
@@ -17,10 +19,10 @@ export default async function Nav() {
               <SideMenu regions={regions} />
               <div className="ml-4 flex items-center">
                 <div className={styles.container}>
-                  <div className={styles.logoText}>
-                    <span className={styles.mainText}>Welcome to Royal Gems Accessories</span>
+                                    <div className={`${styles.logoText} ${styles.textGold}`}>
+                    <span className={`${styles.mainText} font-bold`}>Welcome to Royal Gems Accessories</span>
                     <br />
-                    <span className={styles.subText}>Est 2021</span>
+                    <span className={`${styles.subText} font-bold`}>Est 2021</span>
                   </div>
                 </div>
               </div>
@@ -30,7 +32,7 @@ export default async function Nav() {
             <div className="hidden small:flex items-center gap-x-6 h-full">
               {process.env.FEATURE_SEARCH_ENABLED && (
                 <LocalizedClientLink
-                  className="hover:text-ui-fg-base"
+                  className="hover:text-ui-fg-base font-bold text-gold"
                   href="/search"
                   scroll={false}
                   data-testid="nav-search-link"
@@ -38,18 +40,17 @@ export default async function Nav() {
                   Search
                 </LocalizedClientLink>
               )}
-              <LocalizedClientLink
-                className="hover:text-ui-fg-base"
-                href="/account"
-                data-testid="nav-account-link"
-              >
-                Account
+              <LocalizedClientLink className="hover:text-ui-fg-base font-bold text-gold" href="/account" data-testid="nav-account-link">
+                <div className={`${styles.container} flex items-center`}>
+                  <FontAwesomeIcon icon={faUser} style={{ color: 'gold', width: '25px', height: '25px' }} />
+                  <span className={`${styles.subText} text-gold font-bold`}>Login & Register</span>
+                </div>
               </LocalizedClientLink>
             </div>
             <Suspense
               fallback={
                 <LocalizedClientLink
-                  className="hover:text-ui-fg-base flex gap-2"
+                  className="hover:text-ui-fg-base flex gap-2 font-bold text-gold"
                   href="/cart"
                   data-testid="nav-cart-link"
                 >
